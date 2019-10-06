c055ee7847c37057fb6e3aaa3d625353deb8c09d
EINNNA <annie_t_@hotmail.com>
2019-10-06 20:41:38 +1100

    files added

--- keys.js
+++ keys.js
@@ -1,6 +1,21 @@
 var axios = require("axios");
 var fs = require("fs");
+var moment = require("moment");
+var nodeSpotifyAPI = require("node-spotify-api");
+var dotEnv = require("dotenv");
 
+function liri(search) {
+    this.search = search;
+
+    this.findConcert = function() {
+        axios.get()
+    }
+
+
+
+
+    
+}
 
 
 
--- liri.js
+++ liri.js
@@ -1,26 +1,31 @@
-var dotenv = require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-
-
-var command = process.argv[2];
+var command = process.argv[2]; //
 var search = process.argv.slice(3).join(" ");
 
 
 switch (command) {
-    case "actor":
-        console.log("Searching for an actor");
-        var newActor = new TV(search);
-        newActor.findActor();
+    case "concert-this":
+        console.log("Looking for the concert!");
+        var newConcert = new Liri(search);
     break;
     
-    case "tv":
-        console.log("Searching for an TV Show");
+    case "spotify-this-song":
+        console.log("Looking for the song!");
+        var newSong = new Liri(search);
+
+    break;
+
+    case "movie-this":
+        console.log("Looking for the movie!");
+        var newMovie = new Liri(search);
+
+    break;
+
+    case "do-what-it-says":
+        console.log("Doing something");
         var newTV = new TV(search);
-        newTV.findShow();
+
     break;
 
     default:
-        console.log("Not an option")
-
+        console.log("Not an option, please pick concert-this, spotify-this-song, movie-this or do-what-it-says ")
     }
